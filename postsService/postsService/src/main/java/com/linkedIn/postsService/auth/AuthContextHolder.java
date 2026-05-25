12bb68938beb522cc6c557a6cd4b199de3070044
surajNarale4 <rajnarale415@gmail.com>
2026-05-25 14:24:42 +0530

    AuthContextHandler added

--- postsService/postsService/src/main/java/com/linkedIn/postsService/auth/AuthContextHolder.java
+++ postsService/postsService/src/main/java/com/linkedIn/postsService/auth/AuthContextHolder.java
@@ -1,4 +1,18 @@
 package com.linkedIn.postsService.auth;
 
 public class AuthContextHolder {
+
+    private final static ThreadLocal<Long> currentUserId =new ThreadLocal<>();
+
+    public static Long getCurrentUserId(){
+        return currentUserId.get();
+    }
+
+    static void setCurrentUserId(Long userId){
+        currentUserId.set(userId);
+    }
+
+    static void clear(){
+        currentUserId.remove();
+    }
 }

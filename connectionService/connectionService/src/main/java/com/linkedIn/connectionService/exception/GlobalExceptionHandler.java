554b4e8d4efc5ae39f12ca25a744af9ed3686a55
surajNarale4 <rajnarale415@gmail.com>
2026-07-12 00:22:53 +0530

    environment variables added as per docker-compose

--- connectionService/connectionService/src/main/java/com/linkedIn/connectionService/exception/GlobalExceptionHandler.java
+++ connectionService/connectionService/src/main/java/com/linkedIn/connectionService/exception/GlobalExceptionHandler.java
@@ -21,7 +21,7 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(apiError, HttpStatus.BAD_REQUEST);
     }
 
-//    @ExceptionHandler(RuntimeException.class)
+    @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<ApiError> handleRuntimeException(RuntimeException ex) {
         ApiError apiError = new ApiError(ex.getLocalizedMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         return new ResponseEntity<>(apiError, HttpStatus.INTERNAL_SERVER_ERROR);
